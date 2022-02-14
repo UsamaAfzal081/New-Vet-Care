29f093aafff1ea2ab018e3bd3a2907235aa7db67
DiViNe <eesidr5@gmail.com>
2022-02-14 20:58:20 +0500

    Added Intent

--- app/src/main/java/com/example/musicplayer/MainActivity.java
+++ app/src/main/java/com/example/musicplayer/MainActivity.java
@@ -10,13 +10,18 @@ import android.widget.Button;
 public class MainActivity extends AppCompatActivity {
 
     private Button btnSignUP;
+    private Button btnAboutUs;
+    private  Button btnContactUs;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
 
-
         setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);
+        btnContactUs=findViewById(R.id.btnContactUs);
+        btnAboutUs=findViewById(R.id.btnAboutUs);
         btnSignUP=findViewById(R.id.btnSignUP);
         btnSignUP.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -24,6 +29,24 @@ public class MainActivity extends AppCompatActivity {
                 Intent intent= new Intent(MainActivity.this,SignUp.class);
                 startActivity(intent);
 
+            }
+        }
+
+
+        );
+        btnAboutUs.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this,AboutUs.class);
+                startActivity(i);
+
+            }
+        });
+        btnContactUs.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent in = new Intent(MainActivity.this , ContactUs.class);
+                        startActivity(in);
             }
         });
     }
