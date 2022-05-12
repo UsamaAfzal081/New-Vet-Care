0e73fdc9b4af6cf27334a989f99b3d4260521401
UsamaAfzal17 <usama53453@gmail.com>
2022-05-12 21:28:27 +0500

    new update

--- app/src/main/java/com/example/musicplayer/dashboard.java
+++ app/src/main/java/com/example/musicplayer/dashboard.java
@@ -9,16 +9,26 @@ import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 public class dashboard extends Activity {
     private Button btnprofile;
+    private Button btnrout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.dashboard);
+        setContentView(R.layout.dashboard);
         btnprofile = findViewById(R.id.btnprofile);
+        btnrout = findViewById(R.id.btnrout);
         btnprofile.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent u = new Intent(dashboard.this, MainActivity2.class);
                 startActivity(u);
+            }
+        });
+        btnrout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent v = new Intent(dashboard.this, MainActivity3.class);
+                startActivity(v);
             }
         });
     }
