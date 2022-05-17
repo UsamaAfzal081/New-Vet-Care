5a323887b5806bce0f6856408dedcd93b28cba21
UsamaAfzal17 <usama53453@gmail.com>
2022-05-17 19:26:55 +0500

    new update

--- app/src/main/java/com/example/musicplayer/MainActivity2.java
+++ app/src/main/java/com/example/musicplayer/MainActivity2.java
@@ -2,7 +2,10 @@ package com.example.musicplayer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity2 extends AppCompatActivity {
 
@@ -10,6 +13,16 @@ public class MainActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        TextView text =findViewById(R.id.personName);
+        text.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i =new Intent(MainActivity2.this,Medacc.class);
+                startActivity(i);
+            }
+        });
     }
+
 
 }
